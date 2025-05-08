a39a93b81807535bed9c0b028d26eff7642c5b67
Edwin HC <edwinrhc@gmail.com>
2025-05-08 11:25:00 -0500

    feat(customer): Se actualizo el puerto 8081 feat(product): Se agrego el ProductRestController y ProductRepository

---
+++ paymentchainparent/businessdomain/product/src/main/java/com/paymentchain/product/controller/ProductRestController.java
@@ -0,0 +1,4 @@
+package com.paymentchain.product.controller;
+
+public class ProductRestController {
+}
---
+++ paymentchainparent/businessdomain/product/src/main/java/com/paymentchain/product/respository/ProductRepository.java
@@ -0,0 +1,4 @@
+package com.paymentchain.product.respository;
+
+public interface ProductRepository {
+}
